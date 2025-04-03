eaf3d12c68c801d89d5098c63235ea7a924d8f9a
Campos <100108519+camposeduardo@users.noreply.github.com>
2025-04-02 21:11:41 -0300

    3157

---
+++ src/3151-3200/bee3157/Main.java
@@ -0,0 +1,12 @@
+package bee3157;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String texto = scanner.next();
+        System.out.println("Nao se refuta ciencia com opiniao");
+        scanner.close();
+    }
+}
